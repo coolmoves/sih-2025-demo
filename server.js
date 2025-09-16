d9c1f39f5d8730f61831a2f9817ec1815ffb9b78
coolmoves <acctrrwwe@gmail.com>
2025-09-16 23:59:42 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.json({"version":"demo"});
+});
+
+io.on('connection', (socket) => {
+  console.log('Peer Connected');
+  
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
